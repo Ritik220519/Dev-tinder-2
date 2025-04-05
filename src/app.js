be8ff09086a0794838473941aa064d0e7b4c337c
Ritik220519 <ritik220519@gmail.com>
2025-04-05 13:40:25 +0530

    req.params dynamic routes

--- src/app.js
+++ src/app.js
@@ -3,31 +3,10 @@ const app = express();
 
 // order of the routes matters a lot
 
-// this will handle the GET call /user
-app.get('/user' , (req , res)=>{
+app.get('/user/:userId/:number/:age' , (req , res)=>{
+    console.log(req.params) // this will give the params in the url
     res.send({name: "Ritik" , lastName : "srivastava"}).status(200)
 })
-// this will handle the POST call /user
-app.post("/user" , async(req , res) =>{
-res.send("successfully send data to database.");
-})
-
-// this will handle the DELETE call /user
-app.delete('/user' , (req, res) =>{
-    res.send("successfully deleted data from database.")
-})
-
-
-
-
-
-
-
-
-// this will match all the http methods API call to this path
-app.use("/node/express", (req, res) => {
-    res.send("hello express js ");
-  });
 
 app.listen(7777, () => {
   console.log("server is listining on port 7777");
