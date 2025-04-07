759a6e321af813b0555af78227ad3822a4af2dfb
Ritik220519 <ritik220519@gmail.com>
2025-04-07 11:06:40 +0530

    multiple routes && next()

--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const app = express();
 
-// order of the routes matters a lot
+// multiple Routes handler
+// app.use('/user' , rh1 , [rh2] , [rh3 , rh4])
 
-app.get('/user/:userId/:number/:age' , (req , res)=>{
-    console.log(req.params) // this will give the params in the url
-    res.send({name: "Ritik" , lastName : "srivastava"}).status(200)
-})
+app.use(
+  "/user",
+  [(req, res , next) => {
+    console.log("1st Response");
+    next();
+    // res.send("Response 1st");
+   
+  },
+  (req, res , next) => {
+    console.log(" Response 2nd");
+    // res.send("2nd Response");
+    next();
+  }],
+  (req, res , next) => {
+    console.log(" Response 3rd");
+    next()
+    // res.send("3rd Response");
+  },
+  (req, res) => {
+    console.log(" Response 4th");
+    res.send("4th Response");
+  }
+);
 
 app.listen(7777, () => {
   console.log("server is listining on port 7777");
