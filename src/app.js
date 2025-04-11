1c33a5ea1d0c771b13e31a201b66537a186549d8
Ritik220519 <ritik220519@gmail.com>
2025-04-12 02:03:31 +0530

    init-database && create sechema and model

--- src/app.js
+++ src/app.js
@@ -1,37 +1,40 @@
 const express = require("express");
+const {connectDB} = require("./config/database");
+const User = require("./model/user")
 const app = express();
-const { adminAuth , userAuth } = require("./middleware/auth");
 
-// handle Authntication for all middleware GET , POST .... request
-app.use("/admin", adminAuth);
+app.post("/signup" , async(req,res)=>{
+  const user = new User({
+    firstName : "John",
+    lastName : "Doe",
+    emailId : "john@gmail.com",
+    password : "password",
+    age : 45,
+    gender : "Male",
 
-// write again and again  Authntication for all routes is not good thats why we use middleware
-app.get("/admin/getAllData", (req, res) => {
-  // const token = "abc123";
-  // const isAdminAuthenticated = token === "abc123";
-  // if (!isAdminAuthenticated) {
-  //   res.status(401).send("Unauthorized");
-  // } else {
-  res.send("all data sent");
-  // }
-});
-app.get("/admin/deleteData", (req, res) => {
-  // const token = "abc123";
-  // const isAdminAuthenticated = token === "abc123";
-  // if (!isAdminAuthenticated) {
-  //   res.status(401).send("Unauthorized");
-  // } else {
-  res.send("All data deleted");
-  // }
-});
+  })
 
-app.get("/user/login" , (req, res)=>{
-  res.send("User login page")
-});
-app.get("/user/data" ,userAuth, (req, res)=>{
-  res.send("User data sent successfully");
-});  
+  try {
+    await user.save();
+    res.send("User created successfully ")
+  } catch (error) {
+    res.status(500).send("Error creating user: " + error.message);
+    
+  }
+ 
+})
 
-app.listen(7777, () => {
-  console.log("server is listining on port 7777");
-});
+
+
+
+connectDB()
+  .then(() => {
+    console.log("database connected successfully...");
+    app.listen(7777, () => {
+      console.log("server is listining on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed..." + err);
+  });
+
